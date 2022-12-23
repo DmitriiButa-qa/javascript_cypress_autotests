2a8506bea640c2d9278192b37e862b375d6f2b71
Dmitrii Buta <119076235+DmitriiButa-qa@users.noreply.github.com>
2022-12-23 17:04:00 +0530

    Update staya_correct_log_pass.js

--- cypress/integration/1-my tests/staya_correct_log_pass.js
+++ cypress/integration/1-my tests/staya_correct_log_pass.js
@@ -4,8 +4,8 @@ describe('Тестирование Staya', function () {
         cy.wait(40000);
         cy.get('.Dec2022FamilyDelivertyPromo__closeButton').click();
         cy.get('.header-bottom__right--link').click();
-        cy.get('.auth-form > form > [type="email"]').type('buta.dmitry@gmail.com');
-        cy.get('.auth-form > form > [type="password"]').type('MisterLit88');
+        cy.get('.auth-form > form > [type="email"]').type('Вставь корректный имейл');
+        cy.get('.auth-form > form > [type="password"]').type('Вставь корректный пароль ');
         cy.get('.auth-form__submit > .s-button__content').click();
         cy.wait(30000);
         cy.contains('Мои заказы')
